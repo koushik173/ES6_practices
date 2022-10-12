f3bd100003211c394a7a31d8ae3b938bf7e57249
koushik173 <cse_2012020173@lus.ac.bd>
2022-10-12 06:46:44 +0600

    parent Class added

--- inheritance.js
+++ inheritance.js
@@ -1,8 +1,24 @@
-class Child{
-    constructor(name){
-        this.name = name;
+class Parent{
+    constructor(){
+        this.fatherName = "Schwerznegger";
     }
 }
 
-const baby = new Child("Roy")
-console.log(baby);
+class Child extends Parent{
+    constructor(name){
+        super();
+        this.name = name;
+    }
+
+    getFullNAme(){
+        return this.name + " " + this.fatherName;
+    }
+}
+
+
+
+const baby = new Child("Arnold")
+const baby2 = new Child("Tom")
+console.log(baby.getFullNAme());
+
+console.log(baby2.getFullNAme());
