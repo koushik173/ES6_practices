5d5d45dd7f4949c95bc7ba3098fd6914122f3af8
koushik173 <cse_2012020173@lus.ac.bd>
2022-10-12 07:42:16 +0600

    null undefine learn

---
+++ nullVSundefine.js
@@ -0,0 +1,13 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+// console.log(add(34,4));
+
+function add(num1, num2=10){
+        console.log(num1+ num2);
+    }
+
+    console.log(add(34));
---
+++ trueFalse.js
@@ -0,0 +1,11 @@
+const age = 0;
+//const name = ""
+
+let name = [];
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
