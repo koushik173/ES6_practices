5f6dd09e2abfa6e3a3f2b9220df3c78b409f3cb7
koushik173 <cse_2012020173@lus.ac.bd>
2022-10-12 06:34:07 +0600

    letCons remove comments

--- letCons.js
+++ letCons.js
@@ -1,70 +1,70 @@
-// const hubby = "ELias Kanchon";
-// // hubby = "Omor sunny ";
-// console.log(hubby);
+const hubby = "ELias Kanchon";
+// hubby = "Omor sunny ";
+console.log(hubby);
 
-// const numbers = [12,14]
-// numbers[0] = 56;
-// console.log(numbers)
-// const hero = {name: "roy", phone: 6656};
-// console.log(hero)
+const numbers = [12,14]
+numbers[0] = 56;
+console.log(numbers)
+const hero = {name: "roy", phone: 6656};
+console.log(hero)
 
-// let patientName = "Korim ChaCha"
-// patientName = "Fatema Khala"
+let patientName = "Korim ChaCha"
+patientName = "Fatema Khala"
 
-// console.log(patientName)
+console.log(patientName)
 
-// function add(num1, num2=0)
-// {
-//     // if(num2== undefined){
-//     //     num2=0;
-//     // }
-//     //num2 = num2 || 0;
-//     return num1 + num2;
-// }
+function add(num1, num2=0)
+{
+    if(num2== undefined){
+        num2=0;
+    }
+    num2 = num2 || 0;
+    return num1 + num2;
+}
 
-// //console.log(add(12)) //print ->>>>> nan
-// console.log(add(12,15))
+//console.log(add(12)) //print ->>>>> nan
+console.log(add(12,15))
 
-// template
+template
 
-// const fName = "Justin"
-// const lname = "TimberLake"
+const fName = "Justin"
+const lname = "TimberLake"
 
-// const  funName = fName +" "+ lname;
-// console.log(funName);
-// let funName2 = `${fName} ${lname} is a bad boy`;
-// funName2 = `${fName} ${20+34+343} is a bad boy`;
-// console.log(funName2);
+const  funName = fName +" "+ lname;
+console.log(funName);
+let funName2 = `${fName} ${lname} is a bad boy`;
+funName2 = `${fName} ${20+34+343} is a bad boy`;
+console.log(funName2);
 
-// const multiline = "I just want see u \n And say sorry. \n I know sorry can not fix the broken glass"
+const multiline = "I just want see u \n And say sorry. \n I know sorry can not fix the broken glass"
 
-// const multiline2 = `
-// what can i do now
-// its very hard for me 
-// i know  i did wrong
-// i regret  too much
-// my whole i know i always miss those when you was with me`
+const multiline2 = `
+what can i do now
+its very hard for me 
+i know  i did wrong
+i regret  too much
+my whole i know i always miss those when you was with me`
 
-// console.log(multiline);
-// console.log(multiline2);
+console.log(multiline);
+console.log(multiline2);
 
 // arrow function
-// const doubl = num => num*2;
+const doubl = num => num*2;
 
-// const mathSum = (num1, num2)=>
-// {
+const mathSum = (num1, num2)=>
+{
 
-// }
-// console.log(doubl(8));
+}
+console.log(doubl(8));
 
 
 // treedots------------>>>>>>>>>>>>>>>>>>>>>>>>
 
-// const age = [9,8,7,9]
-// const age2 = [2,35,45]
+const age = [9,8,7,9]
+const age2 = [2,35,45]
 
-// const all = [...age,...age2]
-// console.log(all);
+const all = [...age,...age2]
+console.log(all);
 
 const takaPOisa = [650,62,85]
-console.log(Math.max(...takaPOisa));
+console.log(Math.max(...takaPOisa)); 
